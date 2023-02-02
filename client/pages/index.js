5a8622343766d1820a9688cd4c6cc39613e593af
adityaiiitr <adityaiiitr@gmail.com>
2023-02-02 11:34:00 +0530

    Initial Setup Completed

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import {Button} from 'antd'
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <Button type="primary">click me</Button>
+        </>
+    );
+}
+
+export default Home;
